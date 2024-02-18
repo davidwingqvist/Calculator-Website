4f260ae68e6dfeeae78a4520b44bb518f3139686
davidw <dw123gorde@gmail.com>
2024-02-18 22:50:10 +0900

    Init
    Initial commit,  multiplication division subtraction addition functional.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import Calculator from "./code/Calculator"
 import './App.css';
+import './code/Calculator.css'
 
 function App() {
+  let calc = new Calculator();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="Main">
+        {calc.renderCalculator()}
+      </div>
   );
 }
 
