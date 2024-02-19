82cc3de56a79eb941b2368dee997fb058fee70c0
davidwingqvist <dw123gorde@gmail.com>
2024-02-19 19:51:08 +0900

    UI update and functionality.
    Added e, pi and n!

--- src/code/Calculator.js
+++ src/code/Calculator.js
@@ -11,7 +11,7 @@ class Calculator extends React.Component{
         this.currentNum = '';
         this.previousNum = '';
 
-        // holds the operation, + * -
+        // holds the operation, + * - %
         this.operation = '';
 
         this.switch = false;
@@ -25,11 +25,19 @@ class Calculator extends React.Component{
         document.getElementById("inputWindow").value = this.currentNum;
     }
 
+    setCurrentNum(number)
+    {
+        this.currentNum = number;
+
+        // Update Input Window.
+        document.getElementById("inputWindow").value = this.currentNum;
+    }
+
     setOperation(ops)
     {
         this.operation = ops;
 
-        if(this.switch === false)
+        if(this.switch === false && this.currentNum !== '')
         {
             this.switch = true;
             this.previousNum = this.currentNum;
@@ -71,6 +79,22 @@ class Calculator extends React.Component{
          document.getElementById("inputWindow").value = this.previousNum;
     }
 
+    doFractional()
+    {
+        if(this.currentNum !== '')
+        {
+            let ret = this.currentNum;
+            for(let i = this.currentNum - 1; i > 0; i--)
+            {
+                ret *= i;
+            }
+
+            this.currentNum = ret;
+            // Update Input Window.
+            document.getElementById("inputWindow").value = this.currentNum;
+        }
+    }
+
     resetState()
     {
         this.num = 0;
@@ -90,21 +114,22 @@ class Calculator extends React.Component{
                     <button className='NumberButton' onClick={this.addToCurrentNum.bind(this, '7')}>7</button>
                     <button className='NumberButton' onClick={this.addToCurrentNum.bind(this, '8')}>8</button>
                     <button className='NumberButton' onClick={this.addToCurrentNum.bind(this, '9')}>9</button>
-                    <button className='OperationButton' onClick={this.setOperation.bind(this, '*')}>*</button>
-                    <button className='NumberButton'>e</button>
+                    <button className='OperationButton' onClick={this.setOperation.bind(this, '*')}>x</button>
+                    <button className='NumberButton' onClick={this.setCurrentNum.bind(this, '2.718281')}>e</button>
                 </table>
                 <table>
                     <button className='NumberButton' onClick={this.addToCurrentNum.bind(this, '4')}>4</button>
                     <button className='NumberButton' onClick={this.addToCurrentNum.bind(this, '5')}>5</button>
                     <button className='NumberButton' onClick={this.addToCurrentNum.bind(this, '6')}>6</button>
                     <button className='OperationButton' onClick={this.setOperation.bind(this, '-')}>-</button>
-                    <button className='NumberButton'>π</button>
+                    <button className='NumberButton' onClick={this.setCurrentNum.bind(this, '3.1415926')}>π</button>
                 </table>
                 <table>
                     <button className='NumberButton' onClick={this.addToCurrentNum.bind(this, '1')}>1</button>
                     <button className='NumberButton' onClick={this.addToCurrentNum.bind(this, '2')}>2</button>
                     <button className='NumberButton' onClick={this.addToCurrentNum.bind(this, '3')}>3</button>
                     <button className='OperationButton' onClick={this.setOperation.bind(this, '+')}>+</button>
+                    <button className='OperationButton' onClick={this.doFractional.bind(this)}>n!</button>
                 </table>
                 <table>
                     <button className='OperationButton' onClick={this.setOperation.bind(this, "÷")}>÷</button>
